b14fb629605316ae58240b05bcfab448dee84ab4
atmosmps <atmos.mps@gmail.com>
2019-11-23 17:36:31 -0300

    Adicionando componente funcional

---
+++ fundamentos-react/src/components/ComponenteFuncional.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => {
+    const aprovados = ['Paula', 'Roberta', 'Gustavo']
+
+    const gerarItens = itens => {
+        return itens.map(item => <li>item</li> )
+    }
+}
