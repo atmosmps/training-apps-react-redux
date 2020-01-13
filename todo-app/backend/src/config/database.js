a7ce97891d11117eb322198694d6ac87dd7b33b1
atmosmps <atmos.mps@gmail.com>
2020-01-13 17:43:09 -0300

    Realizando configuração de base com mongoDB.

--- todo-app/backend/src/config/database.js
+++ todo-app/backend/src/config/database.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose')
+
+// A API de prmisse do Mongoose vai usar a API de Promisses global do Node
+mongoose.Promise = global.Promisse
+module.exports = mongoose.connect('mongodb://localhost/todoapp')
+
+/**
+ * MongoDB NoSQL
+ * Orientado a Documentos(Json é o formato deste documento, formato principal)
+ * 
+ * Escalonamento Horizontal mais facilitado
+ */
