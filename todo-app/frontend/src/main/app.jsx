92ca7956ef14da23090fa0701d11790d720eb6b3
atmosmps <atmos.mps@gmail.com>
2020-01-21 14:20:19 -0300

    Ajustes no arquivo.

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -3,8 +3,12 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 
-export default props => (
+import Menu from '../template/Menu'
+import Routes from './routes'
+
+export default props => ( // Quando usamos o parênteses estamos retornando uma expressão; não é o corpo da função.
     <div className='container'>
-        <h1>Teste</h1>
+        <Menu />
+        <Routes />
     </div>
 )
